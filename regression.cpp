596714b0cc53fa32fe3028c47f80220300304c6d
Ankit Goyal <ankit3goyal@gmail.com>
2013-11-10 19:09:03 -0600

    adding local computation graph read test

--- regression.cpp
+++ regression.cpp
@@ -1,5 +1,35 @@
 #include "Galois/Galois.h"
+#include "Galois/Accumulator.h"
+#include "Galois/Bag.h"
+#include "Galois/Statistic.h"
+#include "Galois/UnionFind.h"
+#include "Galois/Graphs/LCGraph.h"
+#include "Galois/ParallelSTL/ParallelSTL.h"
+#include "llvm/Support/CommandLine.h"
+#include "Galois/Runtime/WorkList.h"
 
-int main(void){
+#include "Lonestar/BoilerPlate.h"
+#include<sys/time.h>
+
+#include <utility>
+#include <algorithm>
+#include <iostream>
+#include "Galois/Statistic.h"
+
+struct SNode{
+        unsigned int data;
+};
+
+typedef Galois::Graph::LC_CSR_Graph<SNode,void> Graph;
+Graph graph;
+
+int main(int argc, char **argv){
+	graph.structureFromFile("/work/02681/ankitg/Galois-2.1.8/inputs/structured/torus5.gr");
+
+	//Initialize the component ids of the nodes with the node number
+	for (Graph::iterator ii = graph.begin(), ei = graph.end(); ii != ei; ++ii) {
+		Graph::GraphNode src = *ii;
+		graph.getData(src, Galois::NONE).data = src;
+	}
 	return 0;
 }
