50b638b53eb684e70e021647bc5fdf8f5674b82a
Prateek Agarwal <prat0318@gmail.com>
2013-11-11 19:53:45 -0600

    add input data

--- sgd.cpp
+++ sgd.cpp
@@ -35,13 +35,14 @@ int main(int argc, char* argv[]) {
 	else if (file == 1) inFile.open("/scratch/01011/xinsui/graphdata/rmat8-2e24.w_edgelist_clean", ifstream::in);
 	else if (file == 2) inFile.open("/scratch/01011/xinsui/graphdata/random4-25.w_edgelist",ifstream::in);
 	else if (file == 3) inFile.open("USA-road-d.NY.gr",ifstream::in);
-	else inFile.open("inputfile", ifstream::in); 
+	else inFile.open("madelon", ifstream::in); 
         if(!inFile.is_open())
         {
 		cout << "Unable to open file graph.txt. \nProgram terminating...\n";
                 return 0;
         }
 	inFile>>n>>d;
+        cout<<"Hello world!"<<endl;
 	
 	vector<int > Y;
 	vector<node > Graph;
@@ -51,10 +52,11 @@ int main(int argc, char* argv[]) {
 	while (inFile >> Y[i])
 	{
 		for (int j=0; j<d; j++) {
+//        cout<<"Hello world!"<<i<<" "<<j<<endl;
 			//inFile >> Graph[i][j];
 			int k;
 			inFile >> k; 
-			if (k!=0) Graph[i].samples[j] = k;
+			if (k!=0) Graph[j].samples[i] = k;
 			Graph[i].w=0;
 		}
 		i++;
@@ -64,17 +66,17 @@ int main(int argc, char* argv[]) {
 	}	
 	inFile.close();
 	cout << "No .of samples=" << n << " No of features=" << d << endl;
-	for (i=0;i<Y.size();i++) cout << Y[i] << "|" ;
-	cout << endl;
-	for (i=0;i<Graph.size();i++) {
-		cout << "Node =" << i << " ||||| ";
-		for (std::map<int, int>::iterator it=Graph[i].samples.begin(); it!=Graph[i].samples.end(); ++it) {
-                	std::cout << it->first << " has " << it->second << " | ";
-		}
-		cout << endl;
-        }
-	double error,val,w_next;
-	//while (error > 1e-6) {
+//	for (i=0;i<Y.size();i++) cout << Y[i] << "|" ;
+//	cout << endl;
+//	for (i=0;i<Graph.size();i++) {
+//		cout << "Node =" << i << " ||||| ";
+//		for (std::map<int, int>::iterator it=Graph[i].samples.begin(); it!=Graph[i].samples.end(); ++it) {
+//                	std::cout << it->first << " has " << it->second << " | ";
+//		}
+//		cout << endl;
+//        }
+	double error = 1,val,w_next;
+	while (error > 1e-6) {
 		for (int j=0;j<n;j++) {
 			error =0;
 			val = 0 - Y[j];
@@ -83,19 +85,20 @@ int main(int argc, char* argv[]) {
 					val = val + (Graph[i].w * Graph[i].samples[j]);
 					//cout << "found" << endl;
 				}
-				//cout << "val=" << val << endl;
+//				cout << "val=" << val << endl;
 			}
 			for (i=0;i<Graph.size();i++) {
 				if  (Graph[i].samples.find(j) != Graph[i].samples.end()) {
 					w_next = Graph[i].w - neta * 2 * Graph[i].samples[j] * val;
 					error = error + w_next - Graph[i].w;
 					Graph[i].w = w_next; 
-					//cout << "w_next=" << w_next << endl; 
+					cout << "w_next=" << w_next << endl; 
 				}
 			}
+                cout<<error<<endl;
 		if (error < 1e-6) break; 
         	}
-	//}
+	}
 	cout << "SGD Completed" << endl;
 	for (i=0;i<Graph.size();i++) {
 		cout << Graph[i].w << endl;
