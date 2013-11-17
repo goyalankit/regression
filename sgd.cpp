340d9e0dfb7949f3e87b65cf85f2d4a6d3f7485a
Prateek Agarwal <prat0318@gmail.com>
2013-11-16 19:34:19 -0600

    normalization of X

--- sgd.cpp
+++ sgd.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 #define pair_int pair< int, int >
-#define neta 0.1
+#define neta 0.01
 
 struct comp {
     bool operator() (const pair_int &a, const pair_int &b) {
@@ -22,7 +22,7 @@ struct comp {
 };
 
 struct node {
-	map<int, int > samples;
+	map<int, double > samples;
 	double w;
 };
 
@@ -45,37 +45,40 @@ int main(int argc, char* argv[]) {
         d++; //Increment d for an empty x (noise)
 	vector<int > Y;
 	vector<node > Graph;
+        vector<int> maxX;
 	Y.resize(n);
 	Graph.resize(d);
+        maxX.assign(d,0);
         double initial_w = 0;
-	int i=0;
+	int i=0; 
 	while (inFile >> Y[i])
 	{
 		for (int j=0; j<d; j++) {
                     if(j == 0){Graph[j].samples[i] = 0; Graph[j].w=initial_w;continue;}
-//        cout<<"Hello world!"<<i<<" "<<j<<endl;
-			//inFile >> Graph[i][j];
 			int k;
 			inFile >> k; 
 			Graph[j].samples[i] = k;
+//                        cout<<k<<"--"<< maxX[j]<<":";
+                        if(k > maxX[j]) maxX[j] = k;
 			Graph[i].w=initial_w;
 		}
 		i++;
 	}
+        //Normalize
+        for(int i = 0; i< d; i++) {
+//            cout<<maxX[i]<<":";
+            for(int j = 0; j < n; j++) {
+                Graph[i].samples[j] /= maxX[i];
+//                cout << Graph[i].samples[j]<<" ";
+                if(i == 0) Graph[i].samples[j] = 0;
+            }
+        }
+        
 	if (i != n) {
 		cout << "File input error" << endl; return 0;
 	}	
 	inFile.close();
 	cout << "No .of samples=" << n << " No of features=" << d << endl;
-//	for (i=0;i<Y.size();i++) cout << Y[i] << "|" ;
-//	cout << endl;
-//	for (i=0;i<Graph.size();i++) {
-//		cout << "Node =" << i << " ||||| ";
-//		for (std::map<int, int>::iterator it=Graph[i].samples.begin(); it!=Graph[i].samples.end(); ++it) {
-//                	std::cout << it->first << " has " << it->second << " | ";
-//		}
-//		cout << endl;
-//        }
 	double val,w_next;
 	while (true) {
 		for (int j=0;j<n;j++) {
@@ -85,16 +88,16 @@ int main(int argc, char* argv[]) {
 					val = val + (Graph[i].w * Graph[i].samples[j]);
 				}
 			}
-                        double sum_w = 0.0;
+//                        double sum_w = 0.0;
 			for (i=0;i<Graph.size();i++) {
 				if  (Graph[i].samples.find(j) != Graph[i].samples.end()) {
-					w_next = Graph[i].w - (double)neta * 2.0 * Graph[i].samples[j] * val;
-					Graph[i].w = w_next; 
-                    sum_w += Graph[i].w;
+					Graph[i].w = Graph[i].w - (double)neta * 2.0 * Graph[i].samples[j] * val;
+//                                        cout<<Graph[i].w<<" ";
+//                    sum_w += Graph[i].w;
 				}
 			}
                         //normalize w
-                        for (i=0;i<Graph.size();i++) Graph[i].w /= sum_w;
+//                        for (i=0;i<Graph.size();i++) Graph[i].w /= sum_w;
                         //error calculation
                         double error = 0.0;
                         for (int j1 = 0; j1 < n; j1++) {
