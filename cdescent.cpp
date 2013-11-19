97b3357171af0a232124f18003b963ba720abdf8
Ankit Goyal <ankit3goyal@gmail.com>
2013-11-19 14:49:49 -0600

    coordinate descent using map

--- cdescent.cpp
+++ cdescent.cpp
@@ -51,13 +51,14 @@ int main(int argc, char* argv[]) {
     std::string line;
     int n,d,src,dest,weight;
     float lambda = lambda_default;
-    std::vector<sparse_array>  X_cols,  X_rows;
+//    std::vector<sparse_array>  X_cols,  X_rows;
+    std::map<pair_int, double> X; //transpose of X
     std::map<pair_int, double> XT; //transpose of X
     std::map<pair_int, double> H; //transpose of X^TX
     std::vector< node > Graph;
     int threads = thread_default;
     int iter = iter_default;
-    inFile.open("inputfile2", ifstream::in);
+    inFile.open("inputfile", ifstream::in);
 
     if(argc > 3) {
         lambda = atof(argv[1]);
@@ -75,8 +76,6 @@ int main(int argc, char* argv[]) {
     vector<int > Y;
     vector<int> maxX;
     Y.resize(n);
-    X_cols.resize(d);
-    X_rows.resize(n);
     Graph.resize(d);
 
 
@@ -90,9 +89,7 @@ int main(int argc, char* argv[]) {
         for (;j<d; j++) {
             int k;
             inFile >> k;
-
-            X_cols[j].add(i,k); //Needs optimization. Adding all values for now
-//          X_rows[i].add(j,k);
+            if(k!=0) X[make_pair(i,j)] = k;
             Graph[j].w = initial_w; //could be removed outside the loop
             Graph[j].id = j;       //""
             if(k > maxX[j]) maxX[j] = k;
@@ -101,14 +98,11 @@ int main(int argc, char* argv[]) {
     }
 
     //calculate the \X^T * \X. This approach may not work for large matrices
-    for(int i=0; i< d ; i++){
-        for(int j=0; j< d; j++){
-            for (int k = 0; k < n; k++) {
-                 double tmp = X_cols[i].values[X_cols[i].idxs[k]] * X_cols[j].values[X_cols[j].idxs[k]];
-                 if(tmp>0){
-                     std::cout << "adding pair "<<i<<", "<<j << std::endl;
-                     H[make_pair(i,j)] += tmp;
-                 }
+    for(int i=0; i< n ; i++){
+        for(int j=0; j< n; j++){
+            for(int k=0; k<d; k++) {
+                double tmp = X[make_pair(k,i)] * X[make_pair(k,j)];
+                if(tmp!=0) H[make_pair(i,j)] += tmp;
             }
         }
     }
@@ -118,7 +112,7 @@ int main(int argc, char* argv[]) {
     for (int ii = 0; ii < d; ii++) {
         double temp = 0;
         for(int k=0; k<n; k++){
-            temp += Y[k] * X_cols[ii].values[X_cols[ii].idxs[k]];
+            temp += Y[k] *  X[make_pair(ii,k)];
         }
         Graph[ii].yx = temp;
     }
@@ -131,8 +125,8 @@ int main(int argc, char* argv[]) {
             for (int m = 0; m < d; m++) {
                 val = val + Graph[m].w * H[make_pair(m,i)];
             }
-            Graph[i].w = (Graph[i].yx - val)/(H[make_pair(i,i)]);
-            cout << "weight for "<< i <<" node" << Graph[i].w << endl;
+            if(H[make_pair(i,i)]!=0) Graph[i].w = (Graph[i].yx - val)/(H[make_pair(i,i)]);
+        //    cout << "weight for "<< i <<" node" << Graph[i].w << endl;
         }
     }
 
@@ -140,11 +134,9 @@ int main(int argc, char* argv[]) {
         cout << Graph[i].w << endl;
     }
 
-
-    /*
-       for(it_type iterator = H.begin(); iterator != H.end(); iterator++) {
-       cout << iterator->first.first << ", " << iterator->first.second << " " << iterator->second << endl;
+      /*  for(it_type iterator = H.begin(); iterator != H.end(); iterator++) {
+           cout << iterator->first.first << ", " << iterator->first.second << " " << iterator->second << endl;
        }
-    */
-    return 0;
+
+ */    return 0;
 }
