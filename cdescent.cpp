a23c4604843bc5ade20ae73314229d6e2f2e67f6
Ankit Goyal <ankit3goyal@gmail.com>
2013-11-18 22:34:31 -0600

    transpose of the matrix

--- cdescent.cpp
+++ cdescent.cpp
@@ -10,7 +10,7 @@
 #include <map>
 #include <queue>
 #include <sys/time.h>
-#include <omp.h>
+//#include <omp.h>
 
 using namespace std;
 #define pair_int pair< int, int >
@@ -26,14 +26,14 @@ struct comp {
 };
 
 struct node {
-	map<int, double > samples;
-	double w;
+    map<int, double > samples;
+    double w;
 };
 
 struct sparse_array {
     std::vector<unsigned int> idxs;
-    std::vector<double> values;   
-    
+    std::vector<double> values;
+
     int length() {
         return idxs.size();
     }
@@ -44,72 +44,77 @@ struct sparse_array {
 };
 
 int main(int argc, char* argv[]) {
-	ifstream inFile;
-	std::string line;
-	int n,d,src,dest,weight;
-        float neta = neta_default;
-	//std::map<pair_int, double> XT; //transpose of X
-	std::vector<sparse_array>  X_cols,  X_rows;
-	std::map<pair_int, double> XT; //transpose of X
-	std::map<pair_int, double> H; //transpose of XXt
-        int threads = thread_default;
-        int iter = iter_default;
-	inFile.open("inputfile", ifstream::in);
-        
-        if(argc > 3) {
-            neta = atof(argv[1]);
-            iter = atoi(argv[2]);
-            threads = atoi(argv[3]);
+    ifstream inFile;
+    std::string line;
+    int n,d,src,dest,weight;
+    float neta = neta_default;
+    //std::map<pair_int, double> XT; //transpose of X
+    std::vector<sparse_array>  X_cols,  X_rows;
+    std::map<pair_int, double> XT; //transpose of X
+    std::map<pair_int, double> H; //transpose of XXt
+    int threads = thread_default;
+    int iter = iter_default;
+    inFile.open("inputfile", ifstream::in);
+
+    if(argc > 3) {
+        neta = atof(argv[1]);
+        iter = atoi(argv[2]);
+        threads = atoi(argv[3]);
+    }
+    if(!inFile.is_open())
+    {
+        cout << "Unable to open file graph.txt. \nProgram terminating...\n";
+        return 0;
+    }
+    inFile>>n>>d;
+    cout << "number of nodes " << n << " and featured " << d <<endl;
+
+    vector<int > Y;
+    vector<int> maxX;
+    Y.resize(n);
+    X_cols.resize(d);
+    X_rows.resize(n);
+
+
+    maxX.assign(d,0);
+    double initial_w = 0;
+    int i=0;
+    while (i < n)
+    {
+        inFile >> Y[i];
+        for (int j=0; j<d; j++) {
+            int k;
+            inFile >> k;
+
+            X_cols[j].add(i,k);
+            X_rows[i].add(j,k);
+
+            //			X[make_pair(i,j)] = k;
+            //			XT[make_pair(j,i)] = k;
         }
-        if(!inFile.is_open())
-        {
-		cout << "Unable to open file graph.txt. \nProgram terminating...\n";
-                return 0;
-        }i
-	inFile>>n>>d;
-	cout << "number of nodes " << n << " and featured " << d <<endl;
-
-	vector<int > Y;
-        vector<int> maxX;
-	Y.resize(n);
-	X_cols.resize(d);
-	X_rows.resize(n);
-	
-
-        maxX.assign(d,0);
-        double initial_w = 0;
-	int i=0; 
-	while (i < n)
-	{
-                inFile >> Y[i];
-		for (int j=0; j<d; j++) {
-			int k;
-			inFile >> k; 
-			
-			X_cols[j].add(i,k);			
-			
-			
-//			X_rows[i].add(j,k);
-
-//			X[make_pair(i,j)] = k;
-//			XT[make_pair(j,i)] = k;
-		}
-		i++;
-	}
+        i++;
+    }
 
 
-	for(int i=0; i<X_cols.size ; i++){
-		for(int j=0; j<X_cols.idxs; j++)
-			H[make_pair(i,j)] = X_cols.idA[X_cols[i]]
-	}
-	
-	
-//	cout << "The vertex 2,3 "<< X[make_pair(4,3)] << " in transpose " << XT[make_pair(3,4)] << endl;
-//	for(it_type iterator = X.begin(); iterator != X.end(); iterator++) {
-//		H[make_pair(iterator->first.first,iterator->first.second)] = X[make_pair(iterator->first.first, iterator->first.second)] * XT[make_pair(iterator->first.first, iterator->first.second)];
-//	}
-	
-	
-	
-  	return 0;
+    for(int i=0; i< n ; i++){
+        for(int j=0; j< n; j++){
+            for (int k = 0; k < d; k++) {
+                H[make_pair(i,j)] = H[make_pair(i,j)] + X_rows[i].values[X_rows[i].idxs[k]] * X_rows[j].values[X_rows[j].idxs[k]];
+            }
+        }
+    }
+
+
+    	for(it_type iterator = H.begin(); iterator != H.end(); iterator++) {
+            cout << iterator->first.first << ", " << iterator->first.second << " " << iterator->second << endl;
+        }
+
+    //	cout << "The vertex 2,3 "<< X[make_pair(4,3)] << " in transpose " << XT[make_pair(3,4)] << endl;
+    //	for(it_type iterator = X.begin(); iterator != X.end(); iterator++) {
+    //		H[make_pair(iterator->first.first,iterator->first.second)] = X[make_pair(iterator->first.first, iterator->first.second)] * XT[make_pair(iterator->first.first, iterator->first.second)];
+    //	}
+
+
+
+    return 0;
 }
