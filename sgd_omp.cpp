1544df70838cc8bc028b0609effdec2d953b71d5
Prateek Agarwal <prat0318@gmail.com>
2013-11-23 18:05:08 -0600

    add factor w

--- sgd_omp.cpp
+++ sgd_omp.cpp
@@ -17,6 +17,7 @@ using namespace std;
 #define neta_default .0001
 #define iter_default 10
 #define thread_default 10
+#define is_intercept false
 
 struct comp {
     bool operator() (const pair_int &a, const pair_int &b) {
@@ -33,9 +34,10 @@ int main(int argc, char* argv[]) {
 	ifstream inFile;
 	std::string line;
 	int n,d,src,dest,weight;
-        float neta = neta_default;
-        int threads = thread_default;
-        int iter = iter_default;
+    float intercept = 0.0;
+    float neta = neta_default;
+    int threads = thread_default;
+    int iter = iter_default;
 	inFile.open("inputfile", ifstream::in);
         
         if(argc > 3) {
@@ -49,7 +51,6 @@ int main(int argc, char* argv[]) {
                 return 0;
         }
 	inFile>>n>>d;
-        // d++; //Increment d for an empty x (noise)
 	vector<int > Y;
 	vector<node > Graph;
         vector<int> maxX;
@@ -64,13 +65,8 @@ int main(int argc, char* argv[]) {
 		for (int i=0; i<d; i++) {
 			if(j == 0) Graph[i].w = initial_w;
                         int k = 1;
-                        // if(i == 0) 
-                        //     Graph[i].samples[j] = 1;
-                        // else 
-                        {
-                            inFile >> k; 
-                            Graph[i].samples[j] = k;
-                        }
+                        inFile >> k; 
+                        Graph[i].samples[j] = k;
                         if(k > maxX[i]) maxX[i] = k;
 		}
 		j++;
@@ -90,33 +86,37 @@ int main(int argc, char* argv[]) {
 	cout << "No .of samples=" << n << " No of features=" << d << endl;
         cout << "Neta : "<< neta << " Iterations : "<< iter << " Threads :"<< threads << endl;
 	double val,w_next;
-        int k = 0;
-        int start_s = clock();
+    int k = 0;
+    int start_s = clock();
 	while (k < iter) {
                 k++;
 //                #pragma omp parallel for num_threads(threads)             
 		for (int j = 0; j<n; j++) {
-			val = 0 - Y[j];
+			val = intercept - Y[j];
 			for (int i=0;i<Graph.size();i++) {
 				if  (Graph[i].samples.find(j) != Graph[i].samples.end()) {
 					val = val + (Graph[i].w * Graph[i].samples[j]);
 				}
 			}
-//                        double sum_w = 0.0;
+            if(is_intercept) intercept = intercept - (double)neta * val / n;
+            float max_w = 0.0;
             #pragma omp parallel for num_threads(threads) 
 			for (int i=0;i<Graph.size();i++) {
 				if  (Graph[i].samples.find(j) != Graph[i].samples.end()) {
 					Graph[i].w = Graph[i].w - (double)neta * Graph[i].samples[j] * val / n;
-//                    sum_w += Graph[i].w;
+                    if(Graph[i].w > max_w) max_w = Graph[i].w;
 				}
 			}
                         //normalize w
-//                      for (i=0;i<Graph.size();i++) Graph[i].w /= sum_w;
+                    if(max_w > 10000) {
+                     if(j == 0) cout << "Factor: "<< max_w;
+                     for (int i=0;i<Graph.size();i++) Graph[i].w /= max_w;
+                 }
                         //error calculation
                         if(k== iter && (j > (n-6))) {
                         double error = 0.0;
                         for (int j1 = 0; j1 < n; j1++) {
-                            double partError = 0.0 - Y[j1];
+                            double partError = intercept - Y[j1];
                             for(int i1 = 0; i1 < d; i1++) {
                                 partError = partError + Graph[i1].samples[j1]*Graph[i1].w;
                             }
@@ -130,9 +130,10 @@ int main(int argc, char* argv[]) {
 	}
 	cout << "SGD Completed" << endl;
         cout << "Time taken: " << (clock()-start_s)/double(CLOCKS_PER_SEC)*1000 << " ms." << endl;
-	for (int i=0;i<Graph.size();i++) {
-		cout << Graph[i].w << endl;
-       }
+    // if(is_intercept) cout << intercept << endl;
+	// for (int i=0;i<Graph.size();i++) {
+	// 	cout << Graph[i].w << endl;
+ //       }
 	
   	return 0;
 }
