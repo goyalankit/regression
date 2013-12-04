09a23add2f73952f77f8386570668d2f4a342b8b
Prateek Agarwal <prat0318@gmail.com>
2013-12-04 01:25:46 -0600

    scd

--- modify_inputfile.py
+++ modify_inputfile.py
@@ -13,16 +13,18 @@ X = [content[x].split()[1:] for x in range(1,n)]
 
 A = [[int(X[y][i]) for i in range(len(X[y]))] for y in range(len(X))]
 maxA = max(A)
-print maxA
+#print maxA
 
-Y = [int(content[i].split()[0])/maxA for i in range(1,n)]
+#Y = [int(content[i].split()[0])/maxA for i in range(1,n)]
+Y = [int(content[i].split()[0]) for i in range(1,n)]
 
-# for i in Y:
-# 	print i
+for i in Y:
+ 	print i
 
-Q = [[(i, A[i][j]/maxA) for i in range(len(X)) if A[i][j] != 0] for j in range(len(X[0]))]
+Q = [[(i, A[i][j]) for i in range(len(X)) if A[i][j] != 0] for j in range(len(X[0]))]
 
-# for i in Q:
-#     for j in i:
-#         print j[0],j[1],
-#     print
+#print content[0].strip()
+#for i in Q:
+#    for j in i:
+#        print j[0],j[1],
+#    print
