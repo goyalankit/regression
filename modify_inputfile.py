6550b534a2b3e61646245e402aea5a83ba40db51
Prateek Agarwal <prat0318@gmail.com>
2013-12-03 22:17:47 -0600

    modify

--- modify_inputfile.py
+++ modify_inputfile.py
@@ -5,7 +5,7 @@ with open("madelon") as f:
     content = f.readlines()
 
 n,d = content[0].split()
-n = int(n); d = int(d)
+n = int(n)+1; d = int(d)
 
 Y = [content[i].split()[0] for i in range(1,n)]
 
@@ -13,11 +13,12 @@ X = [content[x].split()[1:] for x in range(1,n)]
 
 A = [[int(X[y][i]) for i in range(len(X[y]))] for y in range(len(X))]
 maxA = max(A)
+print maxA
 
 Y = [int(content[i].split()[0])/maxA for i in range(1,n)]
 
-for i in Y:
-	print i
+# for i in Y:
+# 	print i
 
 Q = [[(i, A[i][j]/maxA) for i in range(len(X)) if A[i][j] != 0] for j in range(len(X[0]))]
 
